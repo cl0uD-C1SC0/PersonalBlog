e8dd6731616e923cf450a58a7f018efe0da42f43
Jose Silva <email@email.com>
2025-05-09 14:46:50 -0300

    <update>: Adicionado arquivo de customização de javascript

---
+++ docs/stylesheets/extra.js
@@ -0,0 +1,7 @@
+// Exemplo de alertas mais interativos
+document.querySelectorAll('.devops-btn').forEach(function(button) {
+  button.addEventListener('click', function() {
+    // Exibe um alerta simples ao clicar
+    alert('Ação DevOps clicada com sucesso!');
+  });
+});
